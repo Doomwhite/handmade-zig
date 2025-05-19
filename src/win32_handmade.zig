78e45df1b0c2d0b05d78b56dc953f432f4131f26
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2025-05-18 23:04:35 -0300

    feat: day 3

--- src/win32_handmade.zig
+++ src/win32_handmade.zig
@@ -11,7 +11,56 @@ const win32 = struct {
 
 pub const UNICODE = true;
 
-pub export fn wWinMain(instance: ?win32.HINSTANCE, prev_instance: ?win32.HINSTANCE, commandline: ?win32.PWSTR, show_code: c_int) c_int {
+pub var bitmap_info: ?win32.BITMAPINFO = null;
+pub var bitmap_memory: ?*?*anyopaque = null;
+pub var bitmap_handle: ?win32.HBITMAP = null;
+pub var bitmap_device_context: ?win32.HDC = null;
+
+// TODO: global for now.
+pub var running = true;
+
+pub fn resizeDIBSection(width: i32, height: i32) void {
+    // TODO: bullet proof this
+    // don't free first, free after, first if that fails
+
+    bitmap_info = .{
+        .bmiHeader = win32.BITMAPINFOHEADER {
+            .biSize = @as(@sizeOf(@TypeOf(bitmap_info.bmiHeader)), u32),
+            .biWidth = width,
+            .biHeight = height,
+            .biPlanes = 1,
+            .biBitCount = 32,
+            .biCompression = @as(win32.BI_RGB, u32),
+            .biXPelsPerMeter = 0,
+            .biYPelsPerMeter = 0,
+            .biClrUsed = 0,
+            .biClrImportant = 0
+        }
+    };
+
+    if(bitmap_device_context == null) {
+        bitmap_device_context = win32.CreateCompatibleDC(0);
+    }
+
+    win32.CreateDIBSection(device_context.?, &bitmap_info, .RGB_COLORS, &bitmap_memory, 0, 0,);
+}
+
+pub fn updateWindow(
+    device_context: ?win32.HDC,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+) void {
+    win32.StretchDIBits(device_context, x, y, width, height, x, y, width, height, lpBits: ?*const anyopaque, lpbmi: ?*const BITMAPINFO, .RGB_COLORS, win32.SRCCOPY,);
+}
+
+pub export fn wWinMain(
+    instance: ?win32.HINSTANCE,
+    prev_instance: ?win32.HINSTANCE,
+    commandline: ?win32.PWSTR,
+    show_code: c_int,
+) c_int {
     // _ = instance;
     _ = prev_instance;
     _ = commandline;
@@ -62,11 +111,16 @@ pub export fn wWinMain(instance: ?win32.HINSTANCE, prev_instance: ?win32.HINSTAN
     );
     assert(window_handle != null);
 
-    while (true) {
+    while (running) {
         win32.OutputDebugStringA("Handle message \n");
 
         var message: win32.MSG = undefined;
-        const message_result = win32.GetMessage(&message, null, 0, 0);
+        const message_result = win32.GetMessage(
+            &message,
+            null,
+            0,
+            0,
+        );
 
         // EXIT/CLOSE
         if (message_result <= 0) break;
@@ -88,16 +142,18 @@ fn WinProc(
 
     switch (message) {
         win32.WM_SIZE => {
+            var client_rect: ?win32.RECT = undefined;
+            _ = win32.GetClientRect(window, &client_rect);
+            assert(client_rect == null);
+
+            const height = client_rect.?.bottom - client_rect.?.top;
+            const width = client_rect.?.right - client_rect.?.left;
+            resizeDIBSection(width, height);
             win32.OutputDebugStringA("WM_SIZE\n");
             std.log.info("WM_SIZE", .{});
         },
-        win32.WM_DESTROY => {
-            win32.OutputDebugStringA("WM_DESTROY\n");
-            std.log.info("WM_DESTROY", .{});
-        },
-        win32.WM_CLOSE => {
-            win32.OutputDebugStringA("WM_CLOSE\n");
-            std.log.info("WM_CLOSE", .{});
+        win32.WM_DESTROY, win32.WM_CLOSE => {
+            running = false;
         },
         win32.WM_ACTIVATEAPP => {
             win32.OutputDebugStringA("WM_ACTIVATEAPP\n");
@@ -107,16 +163,25 @@ fn WinProc(
             std.log.info("WM_PAINT", .{});
             var paint: win32.PAINTSTRUCT = undefined;
 
-            const deviceContext: ?win32.HDC = win32.BeginPaint(window, &paint);
-            if (deviceContext != null) {
+            const device_context: ?win32.HDC = win32.BeginPaint(window, &paint);
+            if (device_context != null) {
                 const x = paint.rcPaint.left;
                 const y = paint.rcPaint.top;
                 const height = paint.rcPaint.bottom - paint.rcPaint.top;
                 const width = paint.rcPaint.right - paint.rcPaint.left;
+
+                updateWindow(
+                    device_context
+                    window,
+                    x,
+                    y,
+                    width,
+                    height,
+                );
                 const color = if (@rem(height, 2) == 0) win32.BLACKNESS else win32.WHITENESS;
 
                 _ = win32.PatBlt(
-                    deviceContext,
+                    device_context,
                     x,
                     y,
                     width,
